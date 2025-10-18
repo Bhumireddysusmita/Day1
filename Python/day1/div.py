15e3dd171d638f4d3648af1e21f60907c75c8291
susi222 <80938461+susi222@users.noreply.github.com>
2025-10-18 09:54:56 +0530

    updated google colab

--- Python/day1/div.py
+++
@@ -1,6 +0,0 @@
-a = 17
-b = 5
-quotient = a // b
-remainder = a % b
-print("Quotient =", quotient)
-print("Remainder =", remainder)
--- Python/day1/keywords.py
+++
@@ -1,4 +0,0 @@
-import keyword
-print("Python Keywords:")
-print(keyword.kwlist)
-print("\nTotal number of keywords:", len(keyword.kwlist))
